b59d985c31fa7eb98dd63ed072e6f3fc7bf2d75c
Promise Femi <promisefemi90@gmail.com>
2023-08-06 05:38:59 +0100

    Updates

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(() => {
+console.log(chrome)
+    // chrome.devtools.network.onRequestFinished.addListener(
+    //     function (request) {
+    //         console.log(request, "REQUEST FROM BANDWIDTH")
+    //     }
+    // )
+
+
+});
