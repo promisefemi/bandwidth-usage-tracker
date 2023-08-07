c39cd670f1b0b5da72b3f4534663052635bd4640
Promise Femi <promisefemi90@gmail.com>
2023-08-07 02:59:26 +0100

    Testing devtools network...  making progress

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-console.log(chrome)
+    console.log(chrome)
     // chrome.devtools.network.onRequestFinished.addListener(
     //     function (request) {
     //         console.log(request, "REQUEST FROM BANDWIDTH")
@@ -8,3 +8,32 @@ console.log(chrome)
 
 
 });
+
+var requestFilter ={
+    // types:[
+    //     "main_frame",
+    //     "sub_frame",
+    //     "script",
+    //     "image",
+    //     "font",
+    //     "object",
+    //     "xmlhttprequest",
+    //     "stylesheet",
+    //     "xmlhttprequest",
+    //     "media",
+    //     "websocket",
+    // ],
+    urls:[
+        "<all_urls>"
+    ]
+};
+
+let extraInfoSec=[
+    // "responseHeaders",
+    "extraHeaders",
+    // "requestBody"
+]
+
+chrome.webRequest.onCompleted.addListener(function (request) {
+    console.log(request, "WEB REQUEST")
+},requestFilter,extraInfoSec)
